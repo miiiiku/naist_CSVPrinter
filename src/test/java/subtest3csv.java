084012cfac8b93b01756edf02a64723866574147
mmmky <w.m124@icloud.com>
2022-08-29 18:45:33 +0900

    nothing sub

--- src/test/java/subtest3csv.java
+++ src/test/java/subtest3csv.java
@@ -1,6 +1,6 @@
-public class Cat extends Test2CSVPrinter_free{
+public class dog extends Test2CSVPrinter_free{
     public static void main(String[] args) {
-        System.out.printf("saikou");
+        System.out.printf("sdfou");
     }
     @Test
     public void ten() throws IOException{
