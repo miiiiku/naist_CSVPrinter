10f47ffa580b6105d2ed235b125ece5824f166c9
mmmky <w.m124@icloud.com>
2022-08-29 17:47:40 +0900

    change String

--- src/test/java/Test2CSVPrinter.java
+++ src/test/java/Test2CSVPrinter.java
@@ -3,36 +3,36 @@ import org.junit.jupiter.api.*;
 import java.io.IOException;
 
 public class Test2CSVPrinter {
-    String name = 'first'
-    int count = 0
-    second()
+    String name = 'first';
+    int count = 0;
+    second();
     @Test
     public void first() throws IOException{
 
-        printDetails(name)
-        second()
+        printDetails(name);
+        second();
     }
     @BeforeAll
     public static void second() throws IOException{
         System.out.println("second");
-        name = 'one'
-        String count1 = getName(count,name)
-        third()
+        name = 'one';
+        String count1 = getName(count,name);
+        third();
     }
     @AfterAll
     public static void third() throws IOException{
         System.out.println("third");
-        four()
+        four();
     }
     @BeforeEach
     public void four() throws IOException{
         System.out.println("four");
-        five()
+        five();
     }
     @AfterEach
     public void five() throws IOException{
         System.out.println("fivve");
-        six()
+        six();
     }
     @Test
     public void six() throws IOException{
@@ -45,18 +45,16 @@ public class Test2CSVPrinter {
 
     public void getName(int count,string name){
 
-        countcheck = count %4
-        bool namecheck = "False"
+        int countcheck = count %4;
+        boolean namecheck = False;
         if (name == "one") {
-            namecheck = "True"
+            namecheck = True;
         }
         if(countcheck && namecheck){
             System.out.println("iine");
         }
         printDetalis(str(count))
-        count +=1
-                Integer i = Integer.valueOf(count);
-        String str = i.toString();
-        return str
+        count +=1;
+        return count;
     }
 }
