eb11fa3bb6699c75e4efb4bb5c187261dab36ec0
mmmky <w.m124@icloud.com>
2022-08-29 18:40:01 +0900

    nothing sub

--- src/test/java/Test2CSVPrinter.java
+++ src/test/java/Test2CSVPrinter.java
@@ -63,8 +63,3 @@ public class Test2CSVPrinter_free {
     }
 }
 
-public class Cat extend Test2CSVPrinter_free{
-    public static void main(String[] args) {
-        System.out.printf("saikou");
-    }
-}
