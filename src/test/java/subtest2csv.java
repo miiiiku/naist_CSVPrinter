3a4ea15639111b86983aae73ba1c09e43af0007c
mmmky <w.m124@icloud.com>
2022-08-29 18:42:55 +0900

    nothing sub

---
+++ src/test/java/subtest2csv.java
@@ -0,0 +1,11 @@
+public class Cat extends Test2CSVPrinter_free{
+    public static void main(String[] args) {
+        System.out.printf("saikou");
+    }
+
+    @Test
+    public void first() throws IOException{
+
+        printDetails(name);
+    }
+}
